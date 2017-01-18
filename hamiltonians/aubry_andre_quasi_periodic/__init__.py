058627f9c9136409bc69887743fe2d2d769a4d66
Mac Lee <macthecadillac@gmail.com>
2017-01-18 00:01:48 -0800

    Updated to reflect changes in this directory

--- hamiltonians/aubry_andre_quasi_periodic/__init__.py
+++ hamiltonians/aubry_andre_quasi_periodic/__init__.py
@@ -1,11 +1,9 @@
 from . import block_diagonalized
-from . import block_diagonalized_obc
 from . import nleg
 from . import nleg_block_diagonalized
 
 __all__ = [
     "block_diagonalized",
-    "block_diagonalized_obc",
     "nleg",
     "nleg_block_diagonalized"
 ]
