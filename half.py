33fc0f3680fefb7e2b0551771234402a6dd719dc
Mac Lee <macthecadillac@gmail.com>
2017-01-19 17:11:04 -0800

    Fixed SizeMismatchError error

--- half.py
+++ half.py
@@ -95,7 +95,8 @@ def expand_and_reorder(N, psi_diag, current_j=0):
         #     psi_ord[to_ord[i], 0] = psi_diag[i, 0]
         veclen = max(psi_diag.shape)
         indices = [to_ord[i] for i in range(veclen)]
-        psi_ord = sp.sparse.csc_matrix((psi_diag, indices, [0, veclen])).toarray()
+        psi_ord = sp.sparse.csc_matrix((psi_diag, indices, [0, veclen]),
+                                       shape=[2 ** N, 1]).toarray()
     # The except suite provides compatibility with 1-D Numpy vectors
     except IndexError:
         for i in psi_diag.nonzero()[0]:
