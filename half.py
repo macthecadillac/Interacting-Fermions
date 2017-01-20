2ed8ec2cc270db67c82c3bf1fbbbd5ec26b54ce3
Mac Lee <macthecadillac@gmail.com>
2017-01-19 17:02:42 -0800

    Experiment changes to expand and reorder

--- half.py
+++ half.py
@@ -88,11 +88,14 @@ def expand_and_reorder(N, psi_diag, current_j=0):
           "current_j" Total <Sz>
     Returns: Numpy 2D array (column vector)
     """
-    psi_ord = np.zeros([2 ** N, 1], complex)
+    # psi_ord = np.zeros([2 ** N, 1], complex)
     to_ord = generate_complete_basis(N, current_j)[2]
     try:
-        for i in psi_diag.nonzero()[0]:
-            psi_ord[to_ord[i], 0] = psi_diag[i, 0]
+        # for i in psi_diag.nonzero()[0]:
+        #     psi_ord[to_ord[i], 0] = psi_diag[i, 0]
+        veclen = max(psi_diag.shape)
+        indices = [to_ord[i] for i in range(veclen)]
+        psi_ord = sp.sparse.csc_matrix((psi_diag, indices, [0, veclen])).toarray()
     # The except suite provides compatibility with 1-D Numpy vectors
     except IndexError:
         for i in psi_diag.nonzero()[0]:
