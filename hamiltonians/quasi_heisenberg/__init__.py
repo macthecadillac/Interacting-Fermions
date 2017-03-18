9662b185cd2f548235a955d34aef58d19c9975c1
Mac Lee <macthecadillac@gmail.com>
2017-03-18 11:34:59 -0700

    Updated to reflect directory changes

--- hamiltonians/quasi_heisenberg/__init__.py
+++ hamiltonians/quasi_heisenberg/__init__.py
@@ -1,9 +1,11 @@
 from . import nleg
+from . import nleg_blkdiag
 from . import oneleg
 from . import oneleg_blkdiag
 
 __all__ = [
     "nleg",
+    "nleg_blkdiag",
     "oneleg",
     "oneleg_blkdiag"
 ]
