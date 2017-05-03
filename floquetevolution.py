620676d570734bfc1f09214adce87c2a277b4816
Thomas Look <thomas.look.546@my.csun.edu>
2017-05-03 16:48:34 -0700

    Initial work on TC Problem

--- floquetevolution.py
+++ floquetevolution.py
@@ -8,25 +8,24 @@ import time
 from spinsys.hamiltonians.ising.one_d_random import H
 from spinsys.hamiltonians.spin_flip.x_field import flip_Sz
 from tempfile import TemporaryFile
+from spinsys.quantities import adj_gap_ratio as agr
 
-N = 4
-H1 = flip_Sz(N, np.pi*.5, 0).tocsc()
-H2 = H(N, 2*np.pi , .05).tocsc()
+N = 8
+H1 = flip_Sz(N, np.pi*2, 0.1).tocsc()
+H2 = H(N, 2*np.pi, .05).tocsc()
 S = s.half.block_diagonalization_transformation(N).toarray()
 psi = np.zeros(2**N)
 psi[np.random.randint((2**N))] = 1
 U1 = expm(-1j*H1)
 U2 = expm(-1j*H2)
 Uf = U2.dot(U1).toarray()
-print(Uf)
+print("Time Evolution Operator", Uf)
 A,B = eig(Uf)
-print(A)
-A = sorted((1j*np.log(A)).real)
-print(A)
-UfB = S.dot(Uf.dot(S.T))
-print(UfB)
-print(H2)
-
+print("RAW Eig", A)
+A = sorted((.5j*np.log(A)).real)
+print("Quasi Eig", A)
+C = np.mean(agr(A))
+print("AGR", C)
 # psi_t = []
 # psi_t.append(psi)
 #
