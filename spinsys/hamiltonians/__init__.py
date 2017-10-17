52ca6ea33fff310e1244d6cdd004a18433db7dc5
Mac Lee <macthecadillac@gmail.com>
2017-10-17 16:38:58 -0700

    Updated import paths

--- spinsys/hamiltonians/__init__.py
+++ spinsys/hamiltonians/__init__.py
@@ -1,5 +1,5 @@
 from . import quasi_heisenberg
 from . import heisenberg
-from . import triangular_lattice_hamitonian
+from . import triangular_lattice_model
 
-__all__ = ['quasi_heisenberg', 'heisenberg', 'triangular_lattice_hamitonian']
+__all__ = ['quasi_heisenberg', 'heisenberg', 'triangular_lattice_model']
