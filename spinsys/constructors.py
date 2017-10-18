95b5d6c0ee155bc947efe7a0b44e1df5a3fc03f9
Mac Lee <macthecadillac@gmail.com>
2017-10-17 22:15:11 -0700

    Striped down SiteVector and created two subclasses that work with Periodic BC and Open BC

--- spinsys/constructors.py
+++ spinsys/constructors.py
@@ -7,21 +7,23 @@ for exact terms and conditions.
 
 
 The constructors module provides several functions that generate
-quantum mechanical operators and also one class that makes traversing
-a lattice simpler. Function included are:
+quantum mechanical operators and also three classes that makes
+traversing a lattice simpler. Functions included are:
     raising
     lowering
     sigmax
     sigmay
     sigmaz
-
-Class:
-    SiteVector
+Classes:
+    SiteVector      # Not meant to be used directly
+    PeriodicBCSiteVector
+    OpenBCSiteVector
 """
 
 import copy
 import numpy as np
 import scipy.sparse as sp
+from spinsys.exceptions import OutOfBoundsError
 
 
 def raising(spin=0.5):
@@ -81,26 +83,65 @@ def sigmaz(spin=0.5):
 
 class SiteVector:
 
-    def __init__(self, ordered_pair, Nx, Ny):
+    def __init__(self, ordered_pair, Nx, Ny, periodic=True):
         self.x = ordered_pair[0]
         self.y = ordered_pair[1]
         self.Nx = Nx
         self.Ny = Ny
+        self.periodic = periodic
 
     def __repr__(self):
         s = "SiteVector(pair=({}, {}), Nx={}, Ny={})"
         return s.format(self.x, self.y, self.Nx, self.Ny)
 
+    def diff(self):
+        raise NotImplementedError
+
+    def __sub__(self, other):
+        """Only as syntactic sugar. The function obviously does not
+        return another SiteVecter
+        """
+        return self.diff(other)
+
     def next_site(self):
+        # This method does not check for bounds so it is possible
+        #  that you'll end up outside of the lattice. Use with care.
         new_index = self.lattice_index + 1
         new_vec = copy.copy(self)
         new_vec.x = new_index % self.Nx
         new_vec.y = new_index // self.Nx
         return new_vec    # Returns a modified instance of self
 
+    @property
+    def lattice_index(self):
+        return self.x + self.Nx * self.y
+
+
+class PeriodicBCSiteVector(SiteVector):
+
+    def __init__(self, ordered_pair, Nx, Ny):
+        super().__init__(ordered_pair, Nx, Ny)
+
+    def diff(self, other):
+        def Δ(diffvar):
+            # Assuming that only short range interaction exists
+            #  (less than half the length of the lattice)
+            d, N = diffvar
+            if round(abs(d) / N) == 0:
+                Δvar = d
+            else:
+                sgn = d // abs(d)
+                Δvar = sgn * N % abs(d)
+            return Δvar
+
+        diffx = (self.x - other.x, self.Nx)
+        diffy = (self.y - other.y, self.Ny)
+        Δx, Δy = Δ(diffx), Δ(diffy)
+        return (Δx, Δy)
+
     def xhop(self, stride):
         new_vec = copy.copy(self)
-        new_vec.x = (self.x + stride) % self.Nx
+        new_vec.y = (self.y + self.Nx * stride) % self.Ny
         return new_vec
 
     def yhop(self, stride):
@@ -108,6 +149,32 @@ class SiteVector:
         new_vec.y = (self.y + self.Nx * stride) % self.Ny
         return new_vec
 
-    @property
-    def lattice_index(self):
-        return self.x + self.Nx * self.y
+
+class OpenBCSiteVector(SiteVector):
+
+    def __init__(self, ordered_pair, Nx, Ny):
+        super().__init__(ordered_pair, Nx, Ny)
+
+    def diff(self, other):
+        """Finds the shortest distance from this site to the other"""
+        Δx = self.x - other.x
+        Δy = self.y - other.y
+        return (Δx, Δy)
+
+    def xhop(self, stride):
+        new_vec = copy.copy(self)
+        new_x = self.x + stride
+        if new_x // self.Nx == self.x // self.Nx:
+            new_vec.x = new_x
+        else:
+            raise OutOfBoundsError("Already at the edge of the lattice")
+        return new_vec
+
+    def yhop(self, stride):
+        new_vec = copy.copy(self)
+        new_y = self.y + self.Nx * stride
+        if new_y // self.Ny == self.x // self.Ny:
+            new_vec.y = new_y
+        else:
+            raise OutOfBoundsError("Already at the edge of the lattice")
+        return new_vec
