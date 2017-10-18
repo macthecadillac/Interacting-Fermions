904c424f845c002b6399da3af0a9d54073de19ed
Mac Lee <macthecadillac@gmail.com>
2017-10-17 22:17:11 -0700

    Changed

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,19 @@
+import numpy as np
 from spinsys.hamiltonians.triangular_lattice_model import hamiltonian
 from scipy import sparse
 import spinsys
 
 
-def correlation(vec, Nx, Ny):
+def structure_factor(vec, k, N):
     vec = sparse.csc_matrix(vec)
-    SiSj = []
+    obsv = []
     for bond in bonds:
-        i, j = bond
-        Si = z_mats[i]
-        Sj = z_mats[j]
-        SiSj.append(Si.dot(Sj))
-    return vec.T.conjugate().dot(sum(SiSj)).dot(vec)[0, 0]
+        site1, site2 = bond
+        i = site1.lattice_index
+        j = site2.lattice_index
+        coeff = np.exp(1j * (k.dot(site1 - site2)))
+        obsv.append(coeff * z_mats[i].dot(z_mats[j]))
+    return 1 / N * vec.T.conjugate().dot(sum(obsv)).dot(vec)[0, 0]
 
 
 Nx = 4
@@ -21,14 +23,16 @@ N = Nx * Ny
 z_mats = [spinsys.half.full_matrix(σz, k, N) for k in range(N)]
 
 bonds = []
-vec = spinsys.constructors.SiteVector((0, 0), Nx, Ny)
+vec = spinsys.constructors.PeriodicBCSiteVector((0, 0), Nx, Ny)
 for i in range(N):
-    bonds.append((vec.lattice_index, vec.xhop(1).lattice_index))
-    bonds.append((vec.lattice_index, vec.yhop(1).lattice_index))
-    bonds.append((vec.lattice_index, vec.xhop(-1).yhop(1).lattice_index))
+    bonds.append((vec, vec.xhop(1)))
+    bonds.append((vec, vec.yhop(1)))
+    bonds.append((vec, vec.xhop(-1).yhop(1)))
     vec = vec.next_site()
 
 H = hamiltonian(Nx, Ny)
 E, ground_state = sparse.linalg.eigsh(H, k=1, which='SA')
-c = correlation(ground_state, Nx, Ny)
-print(c)
+for kx in np.linspace(0, 2 * np.pi, Nx, endpoint=False):
+    for ky in np.linspace(0, 2 * np.pi, Ny, endpoint=False):
+        fac = structure_factor(ground_state, np.array([kx, ky]), N)
+        print((kx, ky), fac)
