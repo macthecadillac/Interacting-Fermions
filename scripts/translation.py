408803b9999524b281a8ce31e64731983a0e903a
Mac Lee <macthecadillac@gmail.com>
2017-10-31 15:44:43 -0700

    Added translation_operator to half.py

--- scripts/translation.py
+++ scripts/translation.py
@@ -1,31 +1,22 @@
 from scipy import sparse
-import numpy as np
 import spinsys
 from hamiltonians.triangular_lattice_model import hamiltonian
 
 
-def translational_operator(Nx, Ny, direction='x'):
-    N = Nx * Ny
-    format_options = '0{}b'.format(N)
-    old_ind = list(range(2 ** N))
-    original_basis = list(map(lambda x: format(x, format_options), old_ind))
-    partitioned = [[i[y * Nx:(y + 1) * Nx] for y in range(Ny)] for i in original_basis]
-    print(partitioned)
-    if direction == 'x':
-        new_basis = []
-        for basis_state in partitioned:
-            new_basis.append([i[-1] + i[:-1] for i in basis_state])
-        # translated_basis = [i[-1] + i[:-1] for i in original_basis]
-        # new_ind = list(map(lambda x: int(x, 2), translated_basis))
-        # A = sparse.csc_matrix((np.ones(2 ** N), (old_ind, new_ind)))
-        # print(new_ind)
-        # print(A.toarray())
-    print(new_basis)
-
-
-Nx = 2
+Nx = 4
 Ny = 3
 N = Nx * Ny
 H = hamiltonian(Nx, Ny)
 E, ψ = sparse.linalg.eigsh(H, k=1, which='SA')
-translational_operator(Nx, Ny)
+ψ = sparse.csc_matrix(ψ)
+T1 = spinsys.half.translation_operator(Nx, Ny, direction='x')
+T2 = T1.dot(T1)
+T3 = T2.dot(T1)
+T4 = spinsys.half.translation_operator(Nx, Ny, direction='y')
+T5 = T4.dot(T4)
+T6 = T5.dot(T4)
+T7 = T4.dot(sparse.linalg.inv(T1))
+T8 = T7.dot(T7)
+T9 = T8.dot(T7)
+for T in [T1, T2, T3, T4, T5, T6, T7, T8, T9]:
+    print(ψ.T.conjugate().dot(T).dot(ψ)[0, 0])
