6a282c8abadffe7c312fd9f22ead6c13a010a9c6
Mac Lee <macthecadillac@gmail.com>
2017-11-01 18:09:24 -0700

    Modified

--- scripts/mac/spectrum.py
+++ scripts/mac/spectrum.py
@@ -7,11 +7,13 @@ from hamiltonians.triangular_lattice_model import hamiltonian
 Nx = 4
 Ny = 4
 N = Nx * Ny
-spectra = np.empty((30, 20))
+nstates = 1000
+neigs = 15
+spectra = np.empty((neigs, nstates))
 t = timer.Timer(20)
-for col, J_ppmm in enumerate(np.linspace(0, 10, 20)):
+for col, J_ppmm in enumerate(np.linspace(0, 10, nstates)):
     H = hamiltonian(Nx, Ny, J_ppmm=J_ppmm)
-    Es = sparse.linalg.eigsh(H, k=30, which='SA', return_eigenvectors=False)
+    Es = sparse.linalg.eigsh(H, k=neigs, which='SA', return_eigenvectors=False)
     spectra[:, col] = Es
     t.progress()
-np.savetxt('spectra_J++--.txt', spectra)
+np.save('spectra_J++--.npy', spectra, allow_pickle=False)
