e01e916f0b7592f21bd5b3993b78a14e159cd650
Mac Lee <macthecadillac@gmail.com>
2017-11-02 13:29:33 -0700

    Started work on dmrg version of the hamiltonian

--- hamiltonians/triangular_lattice_model.py
+++ hamiltonians/triangular_lattice_model.py
@@ -1,5 +1,6 @@
 import numpy as np
-from spinsys import constructors, half
+from scipy import sparse
+from spinsys import constructors, half, dmrg, utils
 
 
 class SiteVector(constructors.PeriodicBCSiteVector):
@@ -23,36 +24,64 @@ class SiteVector(constructors.PeriodicBCSiteVector):
 
 
 def hamiltonian(Nx, Ny, J_pm=1, J_z=1, J_ppmm=1, J_pmz=1):
-    N = Nx * Ny
+    @utils.cache.cache_to_ram
+    def pieces(Nx, Ny):
+        """Generate the reusable pieces of the hamiltonian"""
+        N = Nx * Ny
 
-    σ_p = constructors.raising()
-    σ_m = constructors.lowering()
-    σz = constructors.sigmaz()
+        σ_p = constructors.raising()
+        σ_m = constructors.lowering()
+        σz = constructors.sigmaz()
 
-    p_mats = [half.full_matrix(σ_p, k, N) for k in range(N)]
-    m_mats = [half.full_matrix(σ_m, k, N) for k in range(N)]
-    z_mats = [half.full_matrix(σz, k, N) for k in range(N)]
+        p_mats = [half.full_matrix(σ_p, k, N) for k in range(N)]
+        m_mats = [half.full_matrix(σ_m, k, N) for k in range(N)]
+        z_mats = [half.full_matrix(σz, k, N) for k in range(N)]
 
-    # Permute through all the nearest neighbor coupling bonds
-    bonds = []
-    vec = SiteVector((0, 0), Nx, Ny)
-    for i in range(N):
-        bonds.append((vec, vec.xhop(1)))
-        bonds.append((vec, vec.yhop(1)))
-        bonds.append((vec, vec.xhop(-1).yhop(1)))
-        vec = vec.next_site()
+        # Permute through all the nearest neighbor coupling bonds
+        bonds = []
+        vec = SiteVector((0, 0), Nx, Ny)
+        for i in range(N):
+            bonds.append((vec, vec.xhop(1)))
+            bonds.append((vec, vec.yhop(1)))
+            bonds.append((vec, vec.xhop(-1).yhop(1)))
+            vec = vec.next_site()
 
-    H_pm = H_z = H_ppmm = H_pmz = 0
-    for bond in bonds:
-        site1, site2 = bond
-        i, j = site1.lattice_index, site2.lattice_index
-        γ = np.exp(1j * site1.angle_with(site2))
-        H_pm += p_mats[i].dot(m_mats[j]) + m_mats[i].dot(p_mats[j])
-        H_z += z_mats[i].dot(z_mats[j])
-        H_ppmm += γ * p_mats[i].dot(p_mats[j]) + \
-            γ.conj() * m_mats[i].dot(m_mats[j])
-        H_pmz += 1j * (γ.conj() * z_mats[i].dot(p_mats[j]) -
-                       γ * z_mats[i].dot(m_mats[j]) +
-                       γ.conj() * p_mats[i].dot(z_mats[j]) -
-                       γ * m_mats[i].dot(z_mats[j]))
+        H_pm = H_z = H_ppmm = H_pmz = 0
+        for bond in bonds:
+            site1, site2 = bond
+            i, j = site1.lattice_index, site2.lattice_index
+            γ = np.exp(1j * site1.angle_with(site2))
+            H_pm += p_mats[i].dot(m_mats[j]) + m_mats[i].dot(p_mats[j])
+            H_z += z_mats[i].dot(z_mats[j])
+            H_ppmm += γ * p_mats[i].dot(p_mats[j]) + \
+                γ.conj() * m_mats[i].dot(m_mats[j])
+            H_pmz += 1j * (γ.conj() * z_mats[i].dot(p_mats[j]) -
+                           γ * z_mats[i].dot(m_mats[j]) +
+                           γ.conj() * p_mats[i].dot(z_mats[j]) -
+                           γ * m_mats[i].dot(z_mats[j]))
+        return H_pm, H_z, H_ppmm, H_pmz
+
+    H_pm, H_z, H_ppmm, H_pmz = pieces(Nx, Ny)
     return J_pm * H_pm + J_z * H_z + J_ppmm * H_ppmm + J_pmz * H_pmz
+
+
+class DMRG_Hamiltonian(dmrg.Hamiltonian):
+
+    def __init__(self):
+        super().__init__()
+        self.generators = {
+            '+': constructors.raising(),
+            '-': constructors.lowering(),
+            'z': constructors.sigmaz()
+        }
+
+    def initialize_storage(self):
+        init_block = sparse.csc_matrix(([], ([], [])), shape=[2, 2])
+        init_ops = self.generators
+        self.storage = dmrg.Storage(init_block, init_block, init_ops)
+
+    def newsite_ops(self):
+        pass
+
+    def block_newsite_interaction(self):
+        pass
