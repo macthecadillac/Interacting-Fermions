9668264fa62b1fb60d0382f91fc085f527ca42d8
Mac Lee <macthecadillac@gmail.com>
2017-11-07 18:23:07 -0800

    Adaped H_z to work on a 2D lattice

--- scripts/mac/Tx_hamiltonian.py
+++ scripts/mac/Tx_hamiltonian.py
@@ -1,10 +1,10 @@
+from collections import namedtuple
 import fractions
+import functools
 from itertools import chain
 import numpy as np
 from spinsys import exceptions, utils
-import functools
 from hamiltonians.triangular_lattice_model import SiteVector
-from collections import namedtuple
 
 Shape = namedtuple('shape', 'x, y')
 
@@ -21,12 +21,24 @@ def bond_list(Nx, Ny):
     return bonds
 
 
+def slice_state(state, Nx, Ny):
+    # slice a given state into different "numbers" with each corresponding to
+    #  one leg in the x-direction.
+    n = np.arange(0, Nx * Ny, Nx)
+    return state % (2 ** (n + Nx)) // (2 ** n)
+
+
 def roll_x(state, Nx, Ny):
     """roll to the right"""
     @utils.cache.cache_to_ram
     def xcache(Nx, Ny):
         n = np.arange(0, Nx * Ny, Nx)
-        return (2 ** (n + Nx)), 2 ** n, 2 ** Nx, 2 ** (Nx - 1), 2 ** n
+        a = 2 ** (n + Nx)
+        b = 2 ** n
+        c = 2 ** Nx
+        d = 2 ** (Nx - 1)
+        e = 2 ** n
+        return a, b, c, d, e
 
     # slice state into Ny equal slices
     a, b, c, d, e = xcache(Nx, Ny)
@@ -133,7 +145,7 @@ def generate_dec_to_ind_dictionary(Nx, Ny, kx, ky):
     dec_to_ind = {}
     for i, state in enumerate(states):
         state_shape = Shape(x=state.shape[1], y=state.shape[0])
-        for num in state:
+        for num in state.flatten():
             dec_to_ind[num] = (i, state_shape)
     return states, dec_to_ind
 
@@ -151,62 +163,73 @@ def all_bloch_states(Nx, Ny):
     return states
 
 
-@functools.lru_cache(maxsize=None)
-def count_same_spins(N, state, l, return_loc=False):
-    # edge cases that break down under the kind of binary operation we
-    #  use later
-    if not (state == 2 ** N - 1 or state == 0):
-        # subtraction does a bitwise flip of 1's and 0's. We need this
-        #  because subsequent operations are insensitive to patterns of 0's
-        bit_inverted_state = 2 ** N - 1 - state
-        # the mod operator accounts for periodic boundary conditions
-        couplings = 2 ** np.arange(N) + 2 ** (np.arange(l, N + l) % N)
-        nup = ndown = 0
-        ups = None
-        downs = None
-        for i in couplings:
-            # if a state is unchanged under bitwise OR with the given number,
-            #  it has two 1's at those two sites
-            if state | i == state:
-                nup += 1
-                ups = i
-            # cannot be merged with the previous statement because bit-flipped
-            #  numbers using subtraction are possibly also bit-shifted
-            if bit_inverted_state | i == bit_inverted_state:
-                ndown += 1
-                downs = i
-    elif state == 0:
-        nup, ndown = 0, N
-    else:
-        nup, ndown = N, 0  # when state is all 0's
-
-    if return_loc:
-        return nup, ndown, ups, downs
-    else:
-        return nup, ndown
+def count_same_spins(N, state, l):
+    # subtraction does a bitwise flip of 1's and 0's. We need this
+    #  because subsequent operations are insensitive to patterns of 0's
+    inverted_state = 2 ** N - 1 - state
+    # the mod operator accounts for periodic boundary conditions
+    couplings = 2 ** np.arange(N) + 2 ** (np.arange(l, N + l) % N)
+    nup = ndown = 0
+    ups = downs = None
+    for i in couplings:
+        # if a state is unchanged under bitwise OR with the given number,
+        #  it has two 1's at those two sites
+        if (state | i == state).any():
+            # state is an int when counting in the y-direction while a numpy
+            #  array when counting in the x-direction. This is admittedly an
+            #  ugly hack to make this function, in a sense, "polymorphic".
+            bool_arr = np.array([state | i == state]).flatten()
+            for w, b in enumerate(bool_arr):
+                nup += b
+                if b:
+                    ups = 2 ** (w * N)
+        # cannot be merged with the previous statement because bit-flipped
+        #  numbers using subtraction are possibly also bit-shifted
+        if (inverted_state | i == inverted_state).any():
+            bool_arr = np.array([inverted_state | i == inverted_state]).flatten()
+            for w, b in enumerate(bool_arr):
+                ndown += b
+                if b:
+                    downs = 2 ** (w * N)
+    return nup, ndown, ups, downs
 
 
-def H_z_elements(N, k, i, l):
-    state = generate_dec_to_ind_dictionary(N, k)[0][i][0]
-    nup, ndown = count_same_spins(N, state, l)
-    same_dir = nup + ndown
-    diff_dir = N - same_dir
+def H_z_elements(Nx, Ny, kx, ky, i, l):
+    N = Nx * Ny
+    # i is the index of a certain state in our ordering (which is better
+    # characterized as unordered while deterministic--it always stays the same).
+    # The last [0] part picks out one particular number from the state since any
+    # constituent product state within a Bloch state contains enough information
+    # for us to locate/recreate the entire Bloch state
+    state = generate_dec_to_ind_dictionary(Nx, Ny, kx, ky)[0][i][0, 0]
+    # x-direction
+    sliced_state = slice_state(state, Nx, Ny)
+    nup_x, ndown_x, ups_x, downs_x = count_same_spins(Nx, sliced_state, l)
+    nup_y, ndown_y, ups_y, downs_y = count_same_spins(N, state, l * Nx)
+    same_dir = nup_x + nup_y + ndown_x + ndown_y
+    diff_dir = 2 * N - same_dir
     return 0.25 * (same_dir - diff_dir)
 
 
-def H_pm_elements(N, k, i, l):
-    states, dec_to_ind = generate_dec_to_ind_dictionary(N, k)
+def H_pm_elements(Nx, Ny, kx, ky, i, l):
+    # TODO: unfinished
+    states, dec_to_ind = generate_dec_to_ind_dictionary(Nx, Ny, kx, ky)
     state = states[i][0]
+    print(state)
     i_len = len(states[i])
-    bit1 = 2 ** np.arange(N)
-    bit2 = 2 ** (np.arange(l, N + 1) % N)
-    count = 0  # count of up-downs and down-ups separated by l
-    for b1, b2 in zip(bit1, bit2):
-        if (state | b1 == state) and (not state | b2 == state):
-            count += 1
-        if (not state | b1 == state) and (state | b2 == state):
-            count += 1
-            pm = (b1, b2)
+    N = Nx * Ny
+    d = {'x': Nx, 'y': N}
+    for dn in ['x', 'y']:
+        N = d[dn]
+        bit1 = 2 ** np.arange(N)
+        bit2 = 2 ** (np.arange(l, N + 1) % N)
+        count = 0  # count of up-downs and down-ups separated by l
+        for b1, b2 in zip(bit1, bit2):
+            if (state | b1 == state) and (not state | b2 == state):
+                count += 1
+            if (not state | b1 == state) and (state | b2 == state):
+                count += 1
+                pm = (b1, b2)
     # bit-flip the state once to see what it is coupled to. We don't need
     #  to do it more than once since such flips will always get to
     #  constituent states of the same bloch state
@@ -218,7 +241,7 @@ def H_pm_elements(N, k, i, l):
 
 def H_ppmm_elements(N, k, i, l):
     state = generate_dec_to_ind_dictionary(N, k)[0][i][0]
-    nup, ndown, ups, downs = count_same_spins(N, state, l, return_loc=True)
+    nup, ndown, ups, downs = count_same_spins(N, state, l)
     if ups is not None:
         connected_state = state - ups
     elif downs is not None:
@@ -228,49 +251,52 @@ def H_ppmm_elements(N, k, i, l):
     j, j_len = dec_to_ind[connected_state]
 
 
-Nx = 5
+Nx = 3
 Ny = 4
 
 # print(roll_x(8, Nx, Ny))
 
-N = Nx * Ny
-states = zero_momentum_states(Nx, Ny)
-totlen = 0
-for key, val in states.items():
-    print('\nlengths = {}\ncount = {}\n'.format(key, len(val)))
-    for s in val:
-        p = []
-        for row in s:
-            p.append(list(map(lambda x: format(x, '0{}b'.format(N)), row)))
-        print(p)
-        # print(s, '\n')
-        totlen += np.prod(s.shape)
-print('\nSanity check. Total number of product states:', totlen)
-print('Total number of zero momentum states: {}\n'
-      .format(len(list(chain(*states.values())))))
+# N = Nx * Ny
+# states = zero_momentum_states(Nx, Ny)
+# totlen = 0
+# for key, val in states.items():
+#     print('\nlengths = {}\ncount = {}\n'.format(key, len(val)))
+#     for s in val:
+#         p = []
+#         for row in s:
+#             p.append(list(map(lambda x: format(x, '0{}b'.format(N)), row)))
+#         print(p)
+#         # print(s, '\n')
+#         totlen += np.prod(s.shape)
+# print('\nSanity check. Total number of product states:', totlen)
+# print('Total number of zero momentum states: {}\n'
+#       .format(len(list(chain(*states.values())))))
 
-print('--------------------------------------------------')
-states = all_bloch_states(Nx, Ny)
-for key, val in states.items():
-    print('\nkx, ky = {}\ncount = {}\n'.format(key, len(val)))
-    # for s in val:
-    #     p = []
-    #     for row in s:
-    #         p.append(list(map(lambda x: format(x, '0{}b'.format(N)), row)))
-    #     print(p)
-print('\nTotal number of states:', len(list(chain(*states.values()))))
+# print('--------------------------------------------------')
+# states = all_bloch_states(Nx, Ny)
+# for key, val in states.items():
+#     print('\nkx, ky = {}\ncount = {}\n'.format(key, len(val)))
+#     # for s in val:
+#     #     p = []
+#     #     for row in s:
+#     #         p.append(list(map(lambda x: format(x, '0{}b'.format(N)), row)))
+#     #     print(p)
+# print('\nTotal number of Bloch states:', len(list(chain(*states.values()))))
 
-# N = 5
-# k = 0
-# i = 0
-# state = generate_dec_to_ind_dictionary(N, k)[0][i]
-# print(list(map(lambda x: format(x, '0{}b'.format(N)), state)))
-# print(H_z_elements(N, k, i, 1))
+# N = Nx * Ny
+# kx = ky = 0
+# i = 6
+# l = 2
+# state = generate_dec_to_ind_dictionary(Nx, Ny, kx, ky)[0][i]
+# p = []
+# print('State:')
+# for row in state:
+#     p.append(list(map(lambda x: format(x, '0{}b'.format(N)), row)))
+# print(p)
+# print('Element: {}, l: {}'.format(H_z_elements(Nx, Ny, kx, ky, i, l), l))
 
-# i = 2
-# val, j = H_pm_elements(N, k, i, 1)
-# print(val, j)
-
-# s = int('0110100011100110', 2)
-# print(format(s, '016b'))
-# print(format(roll_state_to_rightx(s, 8, 2), '016b'))
+i = 2
+kx = ky = 0
+l = 1
+val, j = H_pm_elements(Nx, Ny, kx, ky, i, l)
+print(val, j)
