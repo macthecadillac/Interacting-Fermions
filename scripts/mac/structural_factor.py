3eb4677027558e9a92187315e7b50cdcecd6636b
Mac Lee <macthecadillac@gmail.com>
2017-11-08 12:54:36 -0800

    Added variations of J_++-- to the calculation

--- scripts/mac/structural_factor.py
+++ scripts/mac/structural_factor.py
@@ -7,16 +7,19 @@ from hamiltonians.triangular_lattice_model import hamiltonian
 Nx = 4
 Ny = 4
 N = Nx * Ny
-H = hamiltonian(Nx, Ny)
-E, ψ0 = sparse.linalg.eigsh(H, k=1, which='SA')
-ψ0 = sparse.csc_matrix(ψ0)
 nkx = nky = 500
-data = np.empty((nkx, nky), dtype=complex)
-t = utils.timer.Timer(nkx * nky)
-for i, kx in enumerate(np.linspace(-np.pi, np.pi, nkx, endpoint=False)):
-    for j, ky in enumerate(np.linspace(-np.pi, np.pi, nky, endpoint=False)):
-        s = quantities.structural_factor(Nx, Ny, kx, ky, ψ0)
-        data[i, j] = s
-        t.progress()
-np.save('/home/mac/Sync/Research Data/Triangular lattice result/structural_factor_{}x{}.npy'
-        .format(Nx, Ny), data, allow_pickle=False)
+J_z = 1
+J_pm = 0.5
+t = utils.timer.Timer(nkx * nky * 21)
+for J_ppmm in np.linspace(-1, 1, 21):
+    H = hamiltonian(Nx, Ny, J_pm=J_pm, J_z=J_z, J_ppmm=J_ppmm)
+    E, ψ0 = sparse.linalg.eigsh(H, k=1, which='SA')
+    ψ0 = sparse.csc_matrix(ψ0)
+    data = np.empty((nkx, nky), dtype=complex)
+    for i, kx in enumerate(np.linspace(-np.pi, np.pi, nkx, endpoint=False)):
+        for j, ky in enumerate(np.linspace(-np.pi, np.pi, nky, endpoint=False)):
+            s = quantities.structural_factor(Nx, Ny, kx, ky, ψ0)
+            data[i, j] = s
+            t.progress()
+    np.save('./structural_factor_{}x{}_Jz{}_J+-{}_J++--{}.npy'
+            .format(Nx, Ny, J_z, J_pm, J_ppmm), data, allow_pickle=False)
