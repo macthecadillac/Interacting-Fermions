eccfe5e94e8b85316857eecc569974df88b00c33
Mac Lee <macthecadillac@gmail.com>
2017-11-09 16:32:39 -0800

    Added second and third nearest neighbor interactions

--- hamiltonians/triangular_lattice_model.py
+++ hamiltonians/triangular_lattice_model.py
@@ -1,7 +1,8 @@
 import copy
+import functools
 import numpy as np
 from scipy import sparse
-from spinsys import constructors, half, dmrg, utils, exceptions
+from spinsys import constructors, half, dmrg, exceptions
 
 
 class SiteVector(constructors.PeriodicBCSiteVector):
@@ -22,6 +23,78 @@ class SiteVector(constructors.PeriodicBCSiteVector):
                 return 0
         else:
             return 2 * np.pi / 3
+
+    def a1_hop(self, stride):
+        vec = self.xhop(stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def a2_hop(self, stride):
+        vec = self.xhop(-1 * stride).yhop(stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def a3_hop(self, stride):
+        vec = self.yhop(-stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def b1_hop(self, stride):
+        """hop in the a1 - a3 aka b1 direction. Useful for second nearest
+        neighbor coupling interactions
+        """
+        vec = self.xhop(stride).yhop(stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def b2_hop(self, stride):
+        vec = self.xhop(-2 * stride).yhop(stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def b3_hop(self, stride):
+        vec = self.b1_hop(-stride).b2_hop(-stride)
+        if vec == self:
+            raise exceptions.SameSite
+        return vec
+
+    def _neighboring_sites(self, strides, funcs):
+        neighbors = []
+        for stride in strides:
+            for func in funcs:
+                try:
+                    neighbors.append(func(stride))
+                except exceptions.SameSite:
+                    continue
+        return neighbors
+
+    @property
+    def nearest_neighboring_sites(self, all=False):
+        strides = [1, -1] if all else [1]
+        funcs = [self.a1_hop, self.a2_hop, self.a3_hop]
+        return self._neighboring_sites(strides, funcs)
+
+    @property
+    def second_neighboring_sites(self, all=False):
+        """with the all option enabled the method will enumerate all
+        the sites that are second neighbors to the current site.
+        Otherwise it will only enumerate the sites along the b1, b2
+        and b3 directions
+        """
+        strides = [1, -1] if all else [1]
+        funcs = [self.b1_hop, self.b2_hop, self.b3_hop]
+        return self._neighboring_sites(strides, funcs)
+
+    @property
+    def third_neighboring_sites(self, all=False):
+        strides = [2, -2] if all else [2]
+        funcs = [self.a1_hop, self.a2_hop, self.a3_hop]
+        return self._neighboring_sites(strides, funcs)
 
 
 class SemiPeriodicBCSiteVector(SiteVector):
@@ -65,9 +138,78 @@ class SemiPeriodicBCSiteVector(SiteVector):
         return neighbors
 
 
-def hamiltonian(Nx, Ny, J_pm=0, J_z=0, J_ppmm=0, J_pmz=0):
-    """Generates hamiltonian for the triangular lattice model. No
-    optimizations built in.
+def _generate_bonds(Nx, Ny):
+    N = Nx * Ny
+    vec = SiteVector((0, 0), Nx, Ny)
+    bonds_by_order = [[], [], []]
+    for i in range(N):
+        # print(bonds_by_order, '\n\n')
+        nearest_neighbor = vec.nearest_neighboring_sites
+        second_neighbor = vec.second_neighboring_sites
+        third_neighbor = vec.third_neighboring_sites
+        neighbors = [nearest_neighbor, second_neighbor, third_neighbor]
+        for leap, bonds in enumerate(bonds_by_order):
+            for n in neighbors[leap]:
+                bond = (vec, n)
+                bonds.append(bond)
+        vec = vec.next_site()
+    return bonds_by_order
+
+
+@functools.lru_cache(maxsize=None)
+def _gen_full_ops(N):
+    σ_p = constructors.raising()
+    σ_m = constructors.lowering()
+    σz = constructors.sigmaz()
+    p_mats = [half.full_matrix(σ_p, k, N) for k in range(N)]
+    m_mats = [half.full_matrix(σ_m, k, N) for k in range(N)]
+    z_mats = [half.full_matrix(σz, k, N) for k in range(N)]
+    return p_mats, m_mats, z_mats
+
+
+def _gen_z_pm_ops(N, bonds):
+    """generate the H_z and H_pm components of the Hamiltonian"""
+    H_pm = H_z = 0
+    p_mats, m_mats, z_mats = _gen_full_ops(N)
+    for bond in bonds:
+        site1, site2 = bond
+        i, j = site1.lattice_index, site2.lattice_index
+        H_pm += p_mats[i].dot(m_mats[j]) + m_mats[i].dot(p_mats[j])
+        H_z += z_mats[i].dot(z_mats[j])
+    return H_pm, H_z
+
+
+@functools.lru_cache(maxsize=None)
+def _pieces(Nx, Ny):
+    """Generate the reusable pieces of the hamiltonian"""
+    N = Nx * Ny
+    nearest, second, third = _generate_bonds(Nx, Ny)
+    H_pm1, H_z1 = _gen_z_pm_ops(N, nearest)
+    H_pm2, H_z2 = _gen_z_pm_ops(N, second)
+    H_pm3, H_z3 = _gen_z_pm_ops(N, third)
+
+    H_ppmm = H_pmz = 0
+    p_mats, m_mats, z_mats = _gen_full_ops(N)
+    for bond in nearest:
+        site1, site2 = bond
+        i, j = site1.lattice_index, site2.lattice_index
+        γ = np.exp(1j * site1.angle_with(site2))
+
+        H_ppmm += \
+            γ * p_mats[i].dot(p_mats[j]) + \
+            γ.conj() * m_mats[i].dot(m_mats[j])
+
+        H_pmz += 1j * (γ.conj() * z_mats[i].dot(p_mats[j]) -
+                       γ * z_mats[i].dot(m_mats[j]) +
+                       γ.conj() * p_mats[i].dot(z_mats[j]) -
+                       γ * m_mats[i].dot(z_mats[j]))
+
+    return H_pm1, H_z1, H_ppmm, H_pmz, H_pm2, H_z2, H_z3, H_pm3
+
+
+def hamiltonian_dp(Nx, Ny, J_pm=0, J_z=0, J_ppmm=0, J_pmz=0, J2=0, J3=0):
+    """Generates hamiltonian for the triangular lattice model with
+    direct product
 
     Parameters
     --------------------
@@ -75,64 +217,29 @@ def hamiltonian(Nx, Ny, J_pm=0, J_z=0, J_ppmm=0, J_pmz=0):
         number of sites along the x-direction
     Ny: int
         number of sites along the y-direction
-    J_pm: int
-        the J_+- parameter
-    J_z: int
-        the J_z parameter
-    J_ppmm: int
-        the J_++-- parameter
-    J_pmz: int
-        the J_+-z parameter
+    J_pm: float
+        J_+- parameter
+    J_z: float
+        J_z parameter
+    J_ppmm: float
+        J_++-- parameter
+    J_pmz: float
+        J_+-z parameter
+    J2: float
+        second nearest neighbor interaction parameter
+    J3: float
+        third nearest neighbor interaction parameter
 
     Returns
     --------------------
     H: scipy.sparse.csc_matrix
     """
-    @utils.cache.cache_to_ram
-    def pieces(Nx, Ny):
-        """Generate the reusable pieces of the hamiltonian"""
-        N = Nx * Ny
-
-        σ_p = constructors.raising()
-        σ_m = constructors.lowering()
-        σz = constructors.sigmaz()
-
-        p_mats = [half.full_matrix(σ_p, k, N) for k in range(N)]
-        m_mats = [half.full_matrix(σ_m, k, N) for k in range(N)]
-        z_mats = [half.full_matrix(σz, k, N) for k in range(N)]
-
-        # Permute through all the nearest neighbor coupling bonds
-        bonds = []
-        vec = SiteVector((0, 0), Nx, Ny)
-        for i in range(N):
-            bonds.append((vec, vec.xhop(1)))
-            bonds.append((vec, vec.yhop(1)))
-            bonds.append((vec, vec.xhop(-1).yhop(1)))
-            vec = vec.next_site()
-
-        H_pm = H_z = H_ppmm = H_pmz = 0
-        for bond in bonds:
-            site1, site2 = bond
-            i, j = site1.lattice_index, site2.lattice_index
-            γ = np.exp(1j * site1.angle_with(site2))
-
-            H_pm += p_mats[i].dot(m_mats[j]) + m_mats[i].dot(p_mats[j])
-
-            H_z += z_mats[i].dot(z_mats[j])
-
-            H_ppmm += \
-                γ * p_mats[i].dot(p_mats[j]) + \
-                γ.conj() * m_mats[i].dot(m_mats[j])
-
-            H_pmz += 1j * (γ.conj() * z_mats[i].dot(p_mats[j]) -
-                           γ * z_mats[i].dot(m_mats[j]) +
-                           γ.conj() * p_mats[i].dot(z_mats[j]) -
-                           γ * m_mats[i].dot(z_mats[j]))
-
-        return H_pm, H_z, H_ppmm, H_pmz
-
-    H_pm, H_z, H_ppmm, H_pmz = pieces(Nx, Ny)
-    return J_pm * H_pm + J_z * H_z + J_ppmm * H_ppmm + J_pmz * H_pmz
+    H_pm1, H_z1, H_ppmm, H_pmz, H_pm2, H_z2, H_pm3, H_z3 = _pieces(Nx, Ny)
+    nearest_neighbor_terms = J_pm * H_pm1 + J_z * H_z1 + J_ppmm * H_ppmm + \
+        J_pmz * H_pmz
+    second_neighbor_terms = J2 * (H_pm2 + J_z / J_pm * H_z2)
+    third_neighbor_terms = J3 * (H_pm3 + J_z / J_pm * H_z3)
+    return nearest_neighbor_terms + second_neighbor_terms + third_neighbor_terms
 
 
 def hamiltonian_Tx():
